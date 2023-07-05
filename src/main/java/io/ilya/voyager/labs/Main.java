fe1835dc63b7bba5e6740c537182305c15d72a81
ilyaba <ilyaba@gmail.com>
2023-07-05 18:51:26 +0300

    tmp commit

--- src/main/java/io/ilya/voyager/labs/Main.java
+++ src/main/java/io/ilya/voyager/labs/Main.java
@@ -1,7 +1,11 @@
 package io.ilya.voyager.labs;
 
+import io.ilya.voyager.labs.service.HtmlExtractorService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String initialUrl = "https://www.ynetnews.com/";
+        Thread thread = new Thread(new HtmlExtractorService(initialUrl));
+        thread.start();
     }
 }
